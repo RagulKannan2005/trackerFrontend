0f4dd548772c601904e82ca7d4ab542fae54f2d9
RagulKannan2005 <165671801+RagulKannan2005@users.noreply.github.com>
2026-07-28 15:03:10 +0530

    feat: initialize Spring Boot backend and scaffold Angular admin dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Register } from './Authtication/register/register';
 import { Login } from './Authtication/login/login';
 import { Dashboard } from './Admin/dashboard/dashboard';
+import { TeamMember } from './Admin/team-member/team-member';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'register', component: Register },
-  { path: 'login', component: Login },
-  { path: 'admindashboard', component: Dashboard },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'register', component: Register },
+    { path: 'login', component: Login },
+    { path: 'admindashboard', component: Dashboard },
+    { path: 'team-member', component: TeamMember }
 ];
